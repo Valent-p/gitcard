7ec4f059ae10917d81a5a4cd00d3874059d3cd6d
Valent-p <valent.phiri@gmail.com>
2026-01-13 21:58:21 +0200

    Fix: I had to set `as const` on `spring`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export', //This turns it into static HTML files
+    images: {
+    unoptimized: true, //for static export
+  },
 };
 
 export default nextConfig;
